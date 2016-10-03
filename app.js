67380d65448171d57a492fe0a0bcdcb78572f693
kit jonathan ho <j.hokit80@gmail.com>
2016-10-03 14:58:32 +0800

    css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// MY MAIN APP 
+// MY MAIN APP
 
 var express = require('./config/express');
 var mongoose = require('./config/mongoose');
@@ -7,7 +7,7 @@ var mongoose = require('./config/mongoose');
 var db = mongoose();
 var app = express();
 
-app.set('port', (process.env.PORT || 7000));
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), function() {
   console.log('My express server is running at localhost', app.get('port'));
